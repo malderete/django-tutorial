c9a0a51fe82f87191af1d59b230eb20614302d8e
Martin Alderete <malderete@gmail.com>
2014-03-28 16:43:18 -0300

    middleware.py cleaned up

--- apps/core/middleware.py
+++ apps/core/middleware.py
@@ -2,22 +2,12 @@ import logging
 
 
 class MyMiddleware(object):
-
     def process_request(self, request):
-          
         logging.critical("[Miidleware] request. %s" % request.META['PATH_INFO'])
-                    
         return None
-    
+
     def process_response(self, request, response):
-        
         logging.critical("[Middleware] Procesando Request. REQUEST: %s" % request.META['PATH_INFO'])
-                    
         return None
-    
-    
-    def process_response(self, request, response):
-        
-        logging.critical("[Middleware] Procesando Response. RESPONSE: %s" % response.content)
-        
-        return response
+
+
